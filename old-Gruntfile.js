db699094c6c212805ebc6c35097c02360d58b520
Andrew Joia <drewdrewthis@gmail.com>
2016-04-21 22:03:45 -0400

    Set up local dev environment for wp-engine prep

--- old-Gruntfile.js
+++ old-Gruntfile.js
@@ -6,7 +6,12 @@ module.exports = function(grunt) {
             options: {
                 strip: true
             },
-            landing_page_template: {
+            dist: {
+                files: {
+                    'dist/index.html': ['src/index.html']
+                }
+            },
+            landing_template: {
                 files: {
                     'templates/landing-page.php': ['src/index.html']
                 }
@@ -76,18 +81,25 @@ module.exports = function(grunt) {
                 files: ['src/index.html'],
                 tasks: ['processhtml']
             },
+            img: {
+                files: ['src/images/**/*.{png,jpg,gif,ico}'],
+                tasks: ['newer:imagemin']
+            },
             stylesheets: {
-                files: ['src/**/*.scss'],
-                tasks: ['sass']
+                files: ['src/**/*.css', 'src/**/*.scss'],
+                tasks: ['sass', 'cssmin']
             },
             scripts: {
                 files: 'src/**/*.js',
-                tasks: ['jshint']
+                tasks: ['jshint', 'uglify']
+            },
+            karma: {
+                files: ['src/**/*.js', 'tests/**/*.js']
             }
         }
     });
 
-    grunt.registerTask('default', ['processhtml', 'jshint', 'sass']);
+    grunt.registerTask('default', ['newer:imagemin', 'processhtml', 'jshint', 'uglify', 'cssmin', 'sass']);
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-less');
